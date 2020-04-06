e80e1790c1074080a7a01fd79c28481249f49b99
Arjel <konamispica@gmail.com>
2020-04-06 09:37:37 +0800

    Added to dependencies account_cancel

--- omni_gh_account/__manifest__.py
+++ omni_gh_account/__manifest__.py
@@ -19,7 +19,7 @@
 	'version': '0.1',
 
 	# any module necessary for this one to work correctly
-	'depends': ['base','account','sale','account_reports'],
+	'depends': ['base','account','sale','account_reports','account_cancel'],
 
 	# always loaded
 	'data': [
